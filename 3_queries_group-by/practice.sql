c69962360bda4a4e29dcbc610f6039c96eaeafed
cadinkor <elliott.thomlison@gmail.com>
2021-03-30 00:53:20 +0000

    More advanced sql practice

--- 3_queries_group-by/practice.sql
+++ 3_queries_group-by/practice.sql
@@ -12,4 +12,38 @@ HAVING count(*) >= 10
 ORDER BY day;
 
 -- Get all cohorts with 18 or more students.
-SELECT cohorts.name AS cohort_name, count(students.*) AS student_count
+SELECT cohorts.name AS cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >= 18
+ORDER BY student_count;
+
+-- Get the total number of assignment submissions for each cohort.
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
+
+-- Get currently enrolled students' average assignment completion time.
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
+
+-- Get the students who's average time it takes to complete an assignment is less than 
+-- the average estimated time it takes to complete an assignment.
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignments submissions ON student_id = students.id
+JOIN assignment ON assignment.id = assignment.id
+WHERE end_date is NULL
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
+
+
+
